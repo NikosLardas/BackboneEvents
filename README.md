58c280277ab150117e31aece6608df411ba5e632
NikosLardas <nilardas@yahoo.gr>
2022-11-27 21:03:26 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+
+BackboneEvents Application
+
+1. Microsoft SQL Server Database should be created with the name: **BackboneEvents**
---
+++ src/test/java/com/nikos/backboneevents/BackboneEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikos.backboneevents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackboneEventsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
